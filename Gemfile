dc9dc713d9670af2e7e74062729dfa3bb7ce6dce
Steven Soroka <ssoroka78@gmail.com>
2011-02-11 19:44:13 -0600

    add basic rails project

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+# gem 'aws-s3', :require => 'aws/s3'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem "rspec-rails", "~> 2.4"
+end
+
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# gem 'ruby-debug'
+
+# gem 'sqlite3-ruby', :require => 'sqlite3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirePants::Application.config.secret_token = '90d9b7d493092b8682ecacdb38b6678d7f9edfa8026efbd998c72b53bd4cc64e5ae3861d6d8b4a210e9f32f7abfecdadc653ae0f06936e196c508478aa41e740'
